3968f5f323056baab84b545327bff0d7e1e260d3
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2024-12-05 16:53:23 +0100

    Add files via upload

---
+++ Exercice3/src/Grammaire/test.java
@@ -0,0 +1,22 @@
+package Grammaire;
+
+public class test {
+    public static void main(String[] args) {
+        String[] testStrings = {
+            "cdcbc", "bcdcbcb", "cbdcbdcbc", "ccdcbcdcbcdcbcb", "cdcbbb", "cdcb", ""
+        };
+
+        for (String ch : testStrings) {
+            TokenManager tm = new TokenManager(ch);
+            Parseur parseur = new Parseur(tm);
+
+            try {
+                parseur.parse();
+                System.out.println(ch + ": est Valide");
+            } catch (RuntimeException exp) {
+                System.out.println(ch + ": n'est pas Valide");
+                System.out.println("Erreur : " + exp.getMessage());
+            }
+        }
+    }
+}
